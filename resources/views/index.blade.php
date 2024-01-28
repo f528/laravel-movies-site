382aaedc1f0ab77c196f6d4e80a9d3e9e434b1fb
fouzi <faouzi_ee@hotmail.it>
2024-01-28 17:14:43 +0100

    =movie cover with discription created

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,273 @@
             <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
                 Popular Movies
             </h2>
-            <div class="grid grid-cols-4 gap-8">
-                    <div class="mt-8">
-                        <a href="#">
-                            <img src="/assets/7.webp" alt="film">
-                        </a>
+            {{-- cover-movies-grid--}}
+            <div class="grid grid-cols-5 gap-8 ">
 
+                {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/7.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Only Morders</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
                     </div>
-            </div>
+                </div>
+                {{-- end-movie--}}
 
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/5.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">The Bear</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/6.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Oppenheimer</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/10.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Strays</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/14.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Demon Slayer</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/anime.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Guardian of the Galaxy</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/9.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Blue Beath</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/12.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Mario Bros</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/11.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">The rings of Power</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+                   {{-- movie --}}
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/assets/15.webp" alt="film"class="hover:opacity-75 transaction ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+
+                        {{-- discription-movie --}}
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Barby</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <span> <svg class="fill-current text-orange-500" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink"
+                                    width="21px" height="20px">
+                                    <path d="M0,0.054V20h21V0.054H0z M15.422,18.129l-5.264-2.768l-5.265,2.768l1.006-5.863L1.64,8.114l5.887-0.855
+                                 l2.632-5.334l2.633,5.334l5.885,0.855l-4.258,4.152L15.422,18.129z" />
+                                </svg></span>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20,2024</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action.thriller,commedy
+                        </div>
+                    </div>
+                </div>
+                {{-- end-movie--}}
+            </div>
+            {{-- end-cover-movie-grid --}}
         </div>
 
     </div>
