1d4759fac949227d785dcbd43d56a14535a603f5
fouzi <faouzi_ee@hotmail.it>
2024-01-30 20:00:59 +0100

    =fetching api movies and collecting data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('index');
+
+
+        $popolarMovies = Http::withToken(config('services.tmdb.token'))
+        ->get('https://api.themoviedb.org/3/movie/popular')
+        ->json()['results'];
+
+        $genresArray = Http::withToken(config('services.tmdb.token'))
+        ->get('https://api.themoviedb.org/3/genre/movie/list')
+        ->json()['genres'];
+
+        $genres = collect($genresArray)->mapWithKeys(function($genre){
+             return [$genre['id']=> $genre['name']];
+        });
+    dump($genres);
+        return view('movies.index',[
+            'popolarMovies' => $popolarMovies,
+             'genres' => $genres,
+    ]);
     }
 
     public function show()
     {
-        return view('show');
+        return view('movies.show');
     }
 }
